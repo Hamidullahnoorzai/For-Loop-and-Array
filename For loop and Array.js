395c6b81bd5ba94c4236972070c2322b64474a7b
Hamidullahnoorzai <hamidullah.noorzai2025@gmail.com>
2024-09-05 09:29:59 +0430

    for loop and array
    this is Hamidullah Noorzai homework

---
+++ For loop and Array.js
@@ -0,0 +1,24 @@
+
+
+var Computermodels=['Super Computer','Mainframe computer','Mini Computer','Workstation Computer','Personal Computer (PC)','Server Computer','Analog Computer','Digital Computer','Digital Computer'];
+                     //  0                   1                2                     3                  4                      5                6                7                 8        
+//n-1
+//9-1=8
+//for ('initialization','condition','increment/decrement')
+console.log(Computermodels[1]);
+console.log(Computermodels[3]);
+console.log(Computermodels[5]);
+console.log(Computermodels[7]);
+console.log(Computermodels[8]);
+for(i=0;i<Computermodels.length;i++){
+    console.log(Computermodels[i]);
+                                //computermodels[0]
+                                //computermodels[1]
+                                //computermodels[2]
+                                //computermodels[3]
+                                //computermodels[4]
+                                //computermodels[5]
+                                //computermodels[6]
+                                //computermodels[7]
+                                //computermodels[8]
+}
